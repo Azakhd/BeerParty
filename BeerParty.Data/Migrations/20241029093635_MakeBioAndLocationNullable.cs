0fb80ac5d5bd9d092b9541e0264dbae54d5923d1
EVOSandru6 <Azamatfdgfr@gmail.com>
2024-10-29 15:37:34 +0600

    nullable

---
+++ BeerParty.Data/Migrations/20241029093635_MakeBioAndLocationNullable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeerParty.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeBioAndLocationNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "Profiles",
+                nullable: true, // Изменяем на nullable
+                oldClrType: typeof(string),
+                oldType: "text"); // Предположим, что старый тип text
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Profiles",
+                nullable: true, // Изменяем на nullable
+                oldClrType: typeof(string),
+                oldType: "text"); // Предположим, что старый тип text
+        }
+    }
+}
